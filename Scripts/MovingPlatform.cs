496f22737785223d1d87dcbd758be9604380411f
Jakedruth@gmail.com <jakedruth@gmail.com>
2023-11-15 18:21:33 -0500

    Fully implemented moving platforms.

--- Scripts/MovingPlatform.cs
+++ Scripts/MovingPlatform.cs
@@ -1,26 +1,64 @@
 using Godot;
 using System;
 
-[Tool]
 public partial class MovingPlatform : AnimatableBody2D
 {
+    private Path2D _path;
+
     [Export] public float speed;
     [Export] public float waitAtWayPointTime;
     [Export] public bool loop;
-    private int _dir = 1;
+    private int _index;
+    private bool _reverse;
+    private float _timer;
+
+    public override void _Ready()
+    {
+        SyncToPhysics = false;
+        _path = GetParent<Path2D>();
+        if (_path == null)
+            throw new Exception("Could not get Path2D Node from parent. Make sure the parent is of type Path2D");
+        Vector2 pos = _path.Curve.GetPointPosition(0);
+        Position = pos;
+        SyncToPhysics = true;
+    }
 
     public override void _PhysicsProcess(double delta)
     {
+        if (_path == null)
+            return;
 
-    }
+        float dt = (float)delta;
 
-    public override string[] _GetConfigurationWarnings()
-    {
-        Path2D parent = GetParentOrNull<Path2D>();
-        if (parent == null)
+        if (_timer > 0)
         {
-            return new string[] { "Parent must be of type Path2D" };
+            _timer -= dt;
+            return;
         }
-        return base._GetConfigurationWarnings();
+
+        Vector2 target = _path.Curve.GetPointPosition(_index);
+        Position = Position.MoveToward(target, speed * dt);
+
+        if (target == Position)
+        {
+            _timer = waitAtWayPointTime;
+            _index += _reverse ? -1 : 1;
+
+            if (_index >= _path.Curve.PointCount)
+            {
+                if (loop)
+                    _index = 0;
+                else
+                {
+                    _index = _path.Curve.PointCount - 1;
+                    _reverse = true;
+                }
+            }
+            else if (_index < 0)
+            {
+                _index = 0;
+                _reverse = false;
+            }
+        }
     }
 }
